49904d683a4d1c9e4ec27d872adfc54bc1d4b63a
mjkmanikandan88 <mjkmanikandan88@gnail.com>
2020-02-29 13:49:09 +0530

    Manikandan: Initial Commit- Rest controller creation

---
+++ src/main/java/cts/hacker/rabobank/RaboHackerApplication.java
@@ -0,0 +1,19 @@
+package cts.hacker.rabobank;
+
+import java.util.logging.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaboHackerApplication {
+
+	public static void main(String[] args) {
+
+		Logger logger = Logger.getLogger(RaboHackerApplication.class.getName());
+		//System.setProperty("spring.config.name", "addition-server");
+		SpringApplication.run(RaboHackerApplication.class, args);
+		logger.info("HackerApplication Application Started Successfully");
+	}
+
+}
---
+++ src/main/java/cts/hacker/rabobank/controller/CustomerStmtController.java
@@ -0,0 +1,20 @@
+package cts.hacker.rabobank.controller;
+
+import java.util.logging.Logger;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CustomerStmtController {
+
+	Logger logger = Logger.getLogger(CustomerStmtController.class.getName());
+	@PostMapping("/validateCustStmt")
+	public String ValidateCustomerStmt(@RequestParam(value = "customerStmt", defaultValue = "") String customerJson) {
+		logger.info("ValidateCustStmt started..." + customerJson);
+		return customerJson;
+	}
+
+}
